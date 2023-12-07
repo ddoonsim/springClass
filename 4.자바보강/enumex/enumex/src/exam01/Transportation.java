005f81c4a3d94916706216ae776a83af635fd2cf
ddoonsim <2sujg97@naver.com>
2023-12-07 09:17:53 +0900

    12-06 Enum클래스 보충수업 끝

---
+++ 4.자바보강/enumex/enumex/src/exam01/Transportation.java
@@ -0,0 +1,37 @@
+package exam01;
+
+public enum Transportation {
+    BUS("버스") {
+        @Override
+        public int getTotal(int people) {
+            return 1400 * people ;
+        }
+    },
+    SUBWAY("지하철") {
+        @Override
+        public int getTotal(int people) {
+            return 1450 * people ;
+        }
+    },
+    TAXI("택시") {
+        @Override
+        public int getTotal(int people) {
+            return 4500 * people ;
+        }
+    };
+
+    private final String title ;
+
+    // 생성자 정의 가능
+    Transportation(String title) {
+        this.title = title ;
+    }
+
+    // 메서드 정의 가능
+    public String getTitle() {
+        return title;
+    }
+
+    // 추상 메서드 정의 가능
+    public abstract int getTotal(int people) ;
+}
