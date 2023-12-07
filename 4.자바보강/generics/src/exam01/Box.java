905810cc477f80b78026071cd998f3c102073177
ddoonsim <2sujg97@naver.com>
2023-12-07 17:50:30 +0900

    제네릭스 보충

---
+++ 4.자바보강/generics/src/exam01/Box.java
@@ -0,0 +1,16 @@
+package exam01;
+
+/**
+ * 지금은 사용하지 않는 방식
+ */
+public class Box {
+    private Object item ;
+
+    public Object getItem() {
+        return item ;
+    }
+
+    public void setItem(Object item) {
+        this.item = item ;
+    }
+}
