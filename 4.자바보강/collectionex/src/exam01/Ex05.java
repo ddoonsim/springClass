c48c991892ec8771dd69af714330a256836b82ec
ddoonsim <2sujg97@naver.com>
2023-12-12 17:47:05 +0900

    Iterator 반복자 패턴

--- 4.자바보강/collectionex/src/exam01/Ex05.java
+++ 4.자바보강/collectionex/src/exam01/Ex05.java
@@ -1,6 +1,7 @@
 package exam01;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Ex05 {
     public static void main(String[] args) {
@@ -12,6 +13,19 @@ public class Ex05 {
         names.add("이름4") ;
         names.add("이름5") ;
 
+        // 반복자 패턴, 반복은 한 번만 가능
+        Iterator<String> iter = names.iterator() ;
+        while (iter.hasNext()) {
+            String name = iter.next();
+            System.out.println(name);
+        }
+        iter = names.iterator();  // 커서를 다시 요소의 처음으로 이동시킴
+        while (iter.hasNext()) {
+            String name = iter.next();
+            System.out.println(name);
+        }
+
+        System.out.println("-------------------");
         /*
         for (String name : names) {   // 컬렉션과 배열에서 주로 사용되는 향상된 for문
             System.out.println(name);
