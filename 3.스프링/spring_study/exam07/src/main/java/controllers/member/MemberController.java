0cfc06a78a964d1ef61f3a36e1bd84dd6adacf6d
ddoonsim <2sujg97@naver.com>
2023-12-15 12:26:39 +0900

    커맨드 객체와 redirect

--- 3.스프링/spring_study/exam07/src/main/java/controllers/member/MemberController.java
+++ 3.스프링/spring_study/exam07/src/main/java/controllers/member/MemberController.java
@@ -3,8 +3,7 @@ package controllers.member;
 import models.member.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -12,32 +11,44 @@ import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@RequestMapping("/member")    // 공통된 url패턴 정의하고, 하단에서는 /member 명시 X
 public class MemberController {
 
-    @GetMapping("/member/join")
-    public String join(Model model) {
-        // join에만 적용할 스타일과 자바스크립트 파일
-        String[] addCss = {"member/style1", "member/style2"} ;
-        List<String> addScript = Arrays.asList("member/script1", "member/script2") ;
-        model.addAttribute("addCss", addCss) ;
-        model.addAttribute("addScript", addScript) ;
+    @ModelAttribute("hobbies")    // 모든 URL에서 공유할 데이터 설정 시
+    public List<String> hobbies() {
+        return Arrays.asList("자바", "오라클", "JSP", "스프링") ;
+    }
+
+    @GetMapping("/join")   // /member/join으로 매핑
+    //@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})  // 두 가지 매핑 방식 한번에 적용
+    public String join(@ModelAttribute("rjoin") RequestJoin form, Model model) {  // rjoin으로 속성명 바꿈
+
+        // 비어있더라도 객체 생성 필요 : NPE 예외 방지
+        //model.addAttribute("requestJoin", new RequestJoin()) ;  <-- @ModelAttribute 어노테이션으로 자동화
         model.addAttribute("pageTitle", "회원가입") ;
 
         return "member/join" ;
     }
 
-    @PostMapping("/member/join")
-    public String joinPs(RequestJoin form) {
-        System.out.println(form);
-        return "member/join" ;
+
+    @PostMapping("/join")
+    public String joinPs(RequestJoin form, Model model) {
+        //System.out.println(form);
+        //model.addAttribute("requestJoin", form) ;
+        // 커맨드 객체 RequestJoin -> requestJoin 이름으로 Model에 속성이 자동 추가
+            // -> 템플릿 내에서 바로 접근 가능
+
+        // 이동 방식 : response.sendRedirect(request.getContextPath() + "/member/login")
+        // 개발자 도구에서 응답 헤더 중 Location : /exam07/member/login
+        return "redirect:/member/login" ;
     }
 
-    @GetMapping("member/login")
+    @GetMapping("/login")
     public String login() {
         return "member/login" ;
     }
 
-    @PostMapping("member/login")
+    @PostMapping("/login")
     public String loginPs(RequestLogin form) {
         System.out.println(form);
         return "member/login" ;
@@ -46,7 +57,7 @@ public class MemberController {
     /**
      * 멤버 출력 연습
      */
-    @GetMapping("/member/list")
+    @GetMapping("/list")
     public String members(Model model) {
         List<Member> members = new ArrayList<>() ;
         for (int i = 1; i <= 10; i++) {
--- 3.스프링/spring_study/exam07/src/main/java/controllers/member/RequestJoin.java
+++ 3.스프링/spring_study/exam07/src/main/java/controllers/member/RequestJoin.java
@@ -2,6 +2,8 @@ package controllers.member;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * 커맨드 객체 : 기능 요청을 자동으로 처리하고 추가 기능 수행
  */
@@ -13,5 +15,7 @@ public class RequestJoin {
     private String confirmPw ;
     private String userNm ;
     private String email ;
+    private List<String> hobby ;
+    private Address addr ;    // addr 변수를 통해서(addr.addressSub) Address 커맨드 객체를 중첩시킴
     private boolean agree ;
 }
